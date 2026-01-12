c8f7dfca93a49d7c0bbfc25bc4b5fa5e4465d1e2
PRAFUL GUPTA <92764978+prafulthe85@users.noreply.github.com>
2026-01-12 17:11:47 +0000

    callback hell

--- first.js
+++ first.js
@@ -1,13 +1,35 @@
+/////////.    CALLBACK HELL    ///////////
 console.log('first');
 
-function importantAction(username){
+function importantAction(incoming, cb){
     setTimeout(()=>{
-        return `Hello ${username}`
+        cb(`Subscribe to ${incoming}`)
     },1000)
 }
+function likeTheVideo(incoming, cb){
+    setTimeout(()=>{
+        cb(`Like the ${incoming} video`)
+    },500)
+}
 
-const message = importantAction('YEhehehehe')
-console.log(message)
+function shareTheVideo(incoming, cb){
+    setTimeout(()=>{
+        cb(`Share the ${incoming} video`)
+    },500)
+}
+
+// this is how we use callback functions, but below is the nested callback hell
+// so now we will use promises to avoid callback hell
+const message = importantAction('Roadside Coder', function(item){
+    console.log(item);
+    likeTheVideo('JavaScript Interview Questions',function(item) {
+        console.log(item);
+        shareTheVideo('JavaScript Interview Questions', function(item){
+            console.log(item);
+        })
+    })
+}) // using callback will print first second Hello YEhehehehe
+// console.log(message)
 
 console.log('second');
 
