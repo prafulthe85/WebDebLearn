2a732fbfd91a427b9594ac4322406e6371badf28
PRAFUL GUPTA <92764978+prafulthe85@users.noreply.github.com>
2026-01-12 17:49:33 +0000

    02 promises

--- 02_promises.js
+++ 02_promises.js
@@ -94,12 +94,65 @@ function shareTheVideo(incoming){
 
 // Promise.all is a combinator that takes an arr of promises and return an array of promises when all the promises are resolved, if one fails it returns the error of that promise
 
-Promise.all([
-    importantAction('Roadside Coder'),
-    likeTheVideo('JavaScript Interview Questions'),
-    shareTheVideo('JavaScript Interview Questions')
-]).then((res)=>{
-    console.log(res);
-}).catch((err)=>{
-    console.log(err);
-})
+// Promise.all([
+//     importantAction('Roadside Coder'),
+//     likeTheVideo('JavaScript Interview Questions'),
+//     shareTheVideo('JavaScript Interview Questions')
+// ]).then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+// Promise.race is a combinator that takes an arr of promises and return the result of the first settled promise, whether it is resolved or rejected
+
+// Promise.race([
+//     importantAction('Roadside Coder'),
+//     likeTheVideo('JavaScript Interview Questions'),
+//     shareTheVideo('JavaScript Interview Questions')
+// ]).then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+// Promise.allSettled is a combinator that takes an arr of promises and return an array of objects describing the outcome of each promise, whether it is resolved or rejected
+
+// Promise.allSettled([
+//     importantAction('Roadside Coder'),
+//     likeTheVideo('JavaScript Interview Questions'),
+//     shareTheVideo('JavaScript Interview Questions')
+// ]).then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+// Promise.any is a combinator that takes an arr of promises and return the result of the first fulfilled promise, if all promises are rejected it returns an AggregateError
+
+// Promise.any([
+//     importantAction('Roadside Coder'),
+//     likeTheVideo('JavaScript Interview Questions'),
+//     shareTheVideo('JavaScript Interview Questions')
+// ]).then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+
+// Now lets use async await to avoid nesting and make the code look synchronous
+
+const result =  async () =>{
+    try{
+        const message1 = await importantAction('Roadside Coder');
+        console.log(message1);
+        const message2 = await likeTheVideo('JavaScript Interview Questions');
+        console.log(message2);
+        const message3 = await shareTheVideo('JavaScript Interview Questions');
+        console.log(message3);
+    } catch(err){
+        console.log(err);
+    }
+}
+result();
