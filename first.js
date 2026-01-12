61e23eabd72453c5e09c4a8bc762b8d32c3bda5a
PRAFUL GUPTA <92764978+prafulthe85@users.noreply.github.com>
2026-01-12 16:58:53 +0000

    first

--- first.js
+++ first.js
@@ -1,1 +1,14 @@
-console.log('Hello');
+console.log('first');
+
+function importantAction(username){
+    setTimeout(()=>{
+        return `Hello ${username}`
+    },1000)
+}
+
+const message = importantAction('YEhehehehe')
+console.log(message)
+
+console.log('second');
+
+// ouput of above, first undefined second, as setimeout doesn't return spontaneously
